4793a4af9d2ae141a73c3a6bcecfc5ab7d84635f
kong <kong@kongs-MacBook-Pro.local>
2019-06-10 21:03:32 +0700

    [Edit]Add mongoose and bodyprasher server.js file

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
-// Import express
-let express = require('express')
+// Import sections
+let express = require('express');
+let bodyParser = require('body-parser');
+let mongoose = require('mongoose');
 
 // Initialize the app
 let app = express();
+let apiRoutes = require("./api-routes")
+
+// Configure bodyparser to handle post requests
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json());
+
+// Connect to Mongoose and set connection variable
+mongoose.connect('mongodb://localhost/resthub');
+var db = mongoose.connection;
 
 // Setup server port
 var port = process.env.PORT || 80;
@@ -10,7 +24,10 @@ var port = process.env.PORT || 80;
 // Send message for default URL
 app.get('/', (req, res) => res.send('Hello World with Express'));
 
+// Use Api routes in the App
+app.use('/api', apiRoutes)
+
 // Launch app to listen to specified port
 app.listen(port, function () {
-     console.log("Running RestHub on port " + port);
+    console.log("Running RestHub on port " + port);
 });
