28f0b0429748b675596e2a1923850e5c12520ef2
kong <kong@kongs-MacBook-Pro.local>
2019-06-10 21:56:57 +0700

    [Edit]Seperate model controller and router

---
+++ Controller.js
@@ -0,0 +1,18 @@
+Contact = require('./Model');
+
+// Handle index actions
+exports.index = function (req, res) {
+    Contact.get(function (err, contacts) {
+        if (err) {
+            res.json({
+                status: "error",
+                message: err,
+            });
+        }
+        res.json({
+            status: "success",
+            message: "Contacts retrieved successfully",
+            data: contacts
+        });
+    });
+};
--- api-routes.js
+++ api-routes.js
@@ -4,10 +4,13 @@ let router = require('express').Router();
 // Set default API response
 router.get('/', function (req, res) {
     res.json({
-       status: 'API Its Working',
-       message: 'Welcome to RESTHub crafted with love!',
+        status: 'API Its Working',
+        message: 'Welcome to RESTHub crafted with love!',
     });
 });
+var Controller = require('./Controller');
+router.route('/')
+    .get(Controller.index)
 
 // Export API routes
 module.exports = router;
